9f4cf89591bef1c2f050a40c25973c155d633946
Rangat001 <rgtwork114@gmail.com>
2025-06-21 15:30:39 +0530

    Base

---
+++ src/main/java/com/example/rgt/CaseLine/CaseLineApplication.java
@@ -0,0 +1,13 @@
+package com.example.rgt.CaseLine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rgt/CaseLine/CaseLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rgt.CaseLine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
